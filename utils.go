043d72d81b2cf2664577bb604685d4a1ec857c38
Valentin Kuznetsov <vkuznet@gmail.com>
2026-05-09 15:14:25 -0400

    update

--- utils.go
+++ utils.go
@@ -5,9 +5,12 @@ import "fmt"
 // helper function to compose user affiliation from UserInfo
 // First Last, Affliation: Department, University, City, State Zip, Country
 func composeAffiliation(user UserInfo) string {
+	dep := user.Department
+	city := user.City
+	state := user.State
+	zip := user.Zip
+	country := user.Country
 	aff := fmt.Sprintf("%s %s, Affiliation: %s, %s, %s, %s, %s %s, %s",
-		user.FirstName, user.LastName, *user.Department,
-		*user.City, *user.State, *user.Zip, *user.Country)
-
+		user.FirstName, user.LastName, *dep, *city, *state, *zip, *country)
 	return aff
 }
