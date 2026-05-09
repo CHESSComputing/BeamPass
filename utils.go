8059ef8e2e0900e01f0f0efb2869b62442e86d8c
Valentin Kuznetsov <vkuznet@gmail.com>
2026-05-09 15:12:56 -0400

    use pointers for pointers attributes

--- utils.go
+++ utils.go
@@ -6,8 +6,8 @@ import "fmt"
 // First Last, Affliation: Department, University, City, State Zip, Country
 func composeAffiliation(user UserInfo) string {
 	aff := fmt.Sprintf("%s %s, Affiliation: %s, %s, %s, %s, %s %s, %s",
-		user.FirstName, user.LastName, user.Department,
-		user.City, user.State, user.Zip, user.Country)
+		user.FirstName, user.LastName, *user.Department,
+		*user.City, *user.State, *user.Zip, *user.Country)
 
 	return aff
 }
