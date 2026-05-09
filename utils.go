1759e50e124620680f4ccfeebb3913adc21202e4
Valentin Kuznetsov <vkuznet@gmail.com>
2026-05-09 15:40:50 -0400

    handle nil pointers

--- utils.go
+++ utils.go
@@ -1,17 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// helper to safely dereference string pointers
+func strPtr(s *string) string {
+	if s == nil {
+		return ""
+	}
+	return strings.TrimSpace(*s)
+}
 
 // helper function to compose user affiliation from UserInfo
-// First Last, Affliation: Department, University, City, State Zip, Country
+// First Last, Affiliation: Department, University, City, State Zip, Country
 func composeAffiliation(user UserInfo) string {
-	dep := user.Department
-	city := user.City
-	state := user.State
-	org := user.Organization
-	zip := user.Zip
-	country := user.Country
-	aff := fmt.Sprintf("%s %s, Affiliation: %s, %s, %s, %s %s, %s",
-		user.FirstName, user.LastName, *dep, *org, *city, *state, *zip, *country)
-	return aff
+	parts := []string{}
+
+	if v := strPtr(user.Department); v != "" {
+		parts = append(parts, v)
+	}
+
+	if v := strPtr(user.Organization); v != "" {
+		parts = append(parts, v)
+	}
+
+	location := strings.TrimSpace(
+		fmt.Sprintf("%s, %s %s",
+			strPtr(user.City),
+			strPtr(user.State),
+			strPtr(user.Zip),
+		),
+	)
+
+	location = strings.Trim(location, ", ")
+
+	if location != "" {
+		parts = append(parts, location)
+	}
+
+	if v := strPtr(user.Country); v != "" {
+		parts = append(parts, v)
+	}
+
+	affiliation := strings.Join(parts, ", ")
+
+	return fmt.Sprintf(
+		"%s %s, Affiliation: %s",
+		user.FirstName,
+		user.LastName,
+		affiliation,
+	)
 }
